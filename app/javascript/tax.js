f15768073f49822e416842122d9e6e970200e5bb
Fujiwara0805 <tiki4091@gmail.com>
2022-10-10 18:24:56 +0900

    first_commit

--- app/javascript/tax.js
+++ app/javascript/tax.js
@@ -1,14 +1,11 @@
-function tax() {
-  const itemPrice = document.getElementById("item-price");
-    itemPrice.addEventListener('change', () => {
-      const price = itemPrice.value;
-      const addTaxPrice = document.getElementById("add-tax-price");
-      const profit = document.getElementById("profit");
-      const taxPrice = price * 0.1;
-      const saleProfit = price - taxPrice;
-      
-      addTaxPrice.innerHTML = taxPrice;
-      profit.innerHTML = saleProfit; 
-  });
-}
-window.addEventListener("load", tax);
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price")
+    addTaxDom.innerHTML = (Math.floor(inputValue*0.1));
+    const priceFit = document.getElementById("profit")
+      const value_result = inputValue*0.1
+      priceFit.innerHTML = (Math.floor(inputValue - value_result));
+  })  
+});
