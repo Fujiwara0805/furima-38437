1c073edda8418ec95ef5ad16800a0ba3bc027c50
Fujiwara0805 <tiki4091@gmail.com>
2022-10-10 22:04:41 +0900

    third_commit

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,7 +8,7 @@ describe Item, type: :model do
       it '全ての項目が入力されていれば出品ができる' do
         expect(@item).to be_valid
       end
-
+    end
       context '商品出品がうまくいかないとき' do
         it 'imageが空では登録されない' do
           @item.image = nil
@@ -96,6 +96,5 @@ describe Item, type: :model do
           expect(@item.errors.full_messages).to include("Price must be less than 9999999")
         end
       end
-    end
   end
 end  
